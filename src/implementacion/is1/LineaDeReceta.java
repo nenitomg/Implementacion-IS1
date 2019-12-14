cf561aed30b290081c77c1bc66ca700d608a88d9
ManuelGDLVH <manuelgdlvh@gmail.com>
2019-12-14 18:29:21 +0100

    m
    asdasd

--- src/implementacion/is1/LineaDeReceta.java
+++ src/implementacion/is1/LineaDeReceta.java
@@ -5,16 +5,67 @@
  */
 package implementacion.is1;
 
+import java.util.Objects;
+
 /**
  *
  * @author eugen
  */
 public class LineaDeReceta {
+
+    
     private String uMedida;
     private float cantidad;
     public Alimento a;
     
+    
+    public LineaDeReceta(Alimento a, String uMedida, float cantidad){
+        this.a = a;
+        this.uMedida = uMedida;
+        this.cantidad = cantidad;
+    }
+    
+    public String getuMedida() {
+        return uMedida;
+    }
+
+    public void setuMedida(String uMedida) {
+        this.uMedida = uMedida;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Alimento getAlimento() {
+        return a;
+    }
+
+    public void setAlimento(Alimento a) {
+        this.a = a;
+    }
+    
+    public boolean equals(Object o){
+        
+        LineaDeReceta lr = (LineaDeReceta)o;
+        
+        return this.getAlimento().equals(lr.getAlimento());
+    }
+    
+          public int hashCode(){
+        return Objects.hash(this.a.nombre,this.cantidad,this.uMedida);
+          }
+    
+       
     public String toString(){
-        return null;
+        
+        String Str = new String();
+        
+        Str = "Alimento: \n"+ this.getAlimento().toString()+"\nCantidad: "+this.getCantidad()+" "+this.getuMedida();
+        return Str;
     }
 }
